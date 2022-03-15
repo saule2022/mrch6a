b9a221b4fa40dc940f743eb8f9900652905f8fde
saule2022 <99681344+saule2022@users.noreply.github.com>
2022-03-15 22:26:41 +0200

    Tree

--- main/scala/Tree.scala
+++ main/scala/Tree.scala
@@ -1,4 +1,5 @@
 import scala.io.StdIn.readLine
+import scala.util.control.Breaks.break
 
 object Tree extends App {
   //TODO ask person's name
@@ -29,17 +30,43 @@ object Tree extends App {
   //def printTree(name:String, height:Int, symbol:Char='*', maximumHeight:Int=40):Unit = {
   //for this exercise we are not going to adjust maximumHeight
   //TODO print the tree HINT: check the last entry in Day7Strings
-  val name = readLine("Please enter your name:\n")
-  val Range = readLine("How many layer do you a tree?").toInt
-  var number = -1
-  var x = 1
+  val name = readLine("what is your name?"): String
+  val height = readLine("How many layer do you a tree?").toInt
+  var number = 1
+  var i = 0
+  var n = 0
+  println(" " * height + "*")
+  while (i < height - 1) {
+    for (c <- name) {
+      if (i == height - 1) break
+      else {
+        number = number + 2
+        n = n + 1
 
+        println(" " * (height - n) + c.toString * number)
+      }
+      i = i + 1
+    }
 
-  for (x <- 1 to Range) {
-    number = number + 2
-    //Ra=" " *(Range-x)
-
-    println(" " * (Range - x) + "+" * number)
 
   }
+
+
+
+
+
+
+  //val name = readLine("Please enter your name:\n")
+  // val Range = readLine("How many layer do you a tree?").toInt
+  //var number = -1
+  //var x = 1
+
+
+  //for (x <- 1 to Range) {
+  //number = number + 2
+  //Ra=" " *(Range-x)
+
+  //println(" " * (Range - x) + "+" * number)
+
+  //}
 }
