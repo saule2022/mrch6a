59c7ac768d04f28a91520a1d7f6fcaa1a88de8a1
saule2022 <99681344+saule2022@users.noreply.github.com>
2022-03-15 18:30:43 +0200

    Tree

---
+++ main/scala/Tree.scala
@@ -0,0 +1,45 @@
+import scala.io.StdIn.readLine
+
+object Tree extends App {
+  //TODO ask person's name
+  //TODO ask for tree height
+  //TODO print a xmas tree (or another tree) by calling printTree function with the correct parameters
+  //tree height should be the one assigned
+  //simple version for height 3 would be
+  //  *
+  // ***
+  //*****
+
+
+  //for full points I would like to see the following
+  //if user enters name Valdis  and height 9
+  //then we should print
+  //        *
+  //       VVV
+  //      AAAAA
+  //     LLLLLLL
+  //    DDDDDDDDD
+  //   IIIIIIIIIII
+  //  SSSSSSSSSSSSS
+  // VVVVVVVVVVVVVVV
+  //AAAAAAAAAAAAAAAAA
+
+  //let's consider maximum height 40 (so person's name letters could repeat many times)
+
+  //def printTree(name:String, height:Int, symbol:Char='*', maximumHeight:Int=40):Unit = {
+  //for this exercise we are not going to adjust maximumHeight
+  //TODO print the tree HINT: check the last entry in Day7Strings
+  val name = readLine("Please enter your name:\n")
+  val Range = readLine("How many layer do you a tree?").toInt
+  var number = -1
+  var x = 1
+
+
+  for (x <- 1 to Range) {
+    number = number + 2
+    //Ra=" " *(Range-x)
+
+    println(" " * (Range - x) + "+" * number)
+
+  }
+}
