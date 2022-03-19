9f0d7d3fd12411659b1c5b6b2db1e8932abdad89
saule2022 <99681344+saule2022@users.noreply.github.com>
2022-03-19 10:27:21 +0200

    Tree

--- main/scala/Tree.scala
+++ main/scala/Tree.scala
@@ -1,6 +1,4 @@
-import scala.io.StdIn.readLine
 import scala.util.control.Breaks.break
-
 object Tree extends App {
   //TODO ask person's name
   //TODO ask for tree height
@@ -10,8 +8,6 @@ object Tree extends App {
   //  *
   // ***
   //*****
-
-
   //for full points I would like to see the following
   //if user enters name Valdis  and height 9
   //then we should print
@@ -27,46 +23,49 @@ object Tree extends App {
 
   //let's consider maximum height 40 (so person's name letters could repeat many times)
 
-  //def printTree(name:String, height:Int, symbol:Char='*', maximumHeight:Int=40):Unit = {
-  //for this exercise we are not going to adjust maximumHeight
-  //TODO print the tree HINT: check the last entry in Day7Strings
-  val name = readLine("what is your name?"): String
-  val height = readLine("How many layer do you a tree?").toInt
-  var number = 1
-  var i = 0
-  var n = 0
-  println(" " * height + "*")
-  while (i < height - 1) {
-    for (c <- name.toUpperCase()) {
-      if (i == height - 1) break
-      else {
-        number = number + 2
-        n = n + 1
+  def printTree(name: String, height: Int, symbol: Char = '*', maximumHeight: Int = 40): Unit = {
+    //for this exercise we are not going to adjust maximumHeight
+    //TODO print the tree HINT: check the last entry in Day7Strings
+    //val name = readLine("what is your name?"): String
+    // val height = readLine("How many layer do you a tree?").toInt
 
-        println(" " * (height - n) + c.toString * number)
+    var number = 1
+    var i = 0
+    var n = 0
+    if (height == maximumHeight) {
+      val height = maximumHeight
+    } else {
+      println(" " * height + symbol)
+      while (i < height - 1) {
+        for (c <- name.toUpperCase()) {
+          if (i == height - 1) break
+          else {
+            number = number + 2
+            n = n + 1
+
+            println(" " * (height - n) + c.toString * number)
+          }
+          i = i + 1
+        }
       }
-      i = i + 1
     }
+    println(printTree(name = Valdis, 10, '*', 40))
 
 
+
+
+
+    //val name = readLine("Please enter your name:\n")
+    // val Range = readLine("How many layer do you a tree?").toInt
+    //var number = -1
+    //var x = 1
+
+
+    //for (x <- 1 to Range) {
+    //number = number + 2
+    //Ra=" " *(Range-x)
+
+    //println(" " * (Range - x) + "+" * number)
+
+    //}
   }
-
-
-
-
-
-
-  //val name = readLine("Please enter your name:\n")
-  // val Range = readLine("How many layer do you a tree?").toInt
-  //var number = -1
-  //var x = 1
-
-
-  //for (x <- 1 to Range) {
-  //number = number + 2
-  //Ra=" " *(Range-x)
-
-  //println(" " * (Range - x) + "+" * number)
-
-  //}
-}
