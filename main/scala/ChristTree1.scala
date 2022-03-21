22d206322324d88e134b7baff4de24e385fe34af
saule2022 <99681344+saule2022@users.noreply.github.com>
2022-03-21 09:25:19 +0200

    Christmas Tree 1

---
+++ main/scala/ChristTree1.scala
@@ -0,0 +1,20 @@
+import scala.io.StdIn.readLine
+
+object Christtree1 extends App {
+  //TODO ask person's name
+  //TODO ask for tree height
+  //TODO print a xmas tree (or another tree) by calling printTree function with the correct parameters
+  //tree height should be the one assigned
+  //simple version for height 3 would be
+  //  *
+  // ***
+  //*****
+  val name = readLine("Please enter your name:\n")
+  val Range = readLine("How many layers do you want in a tree?").toInt
+  var number = -1
+  var x = 1
+  for (x <- 1 to Range) {
+    number = number + 2
+    println(" " * (Range - x) + "+" * number)
+  }
+}
