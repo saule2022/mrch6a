e581b3ad0c50e922e8f2ac74a9babc4da613d87f
saule2022 <99681344+saule2022@users.noreply.github.com>
2022-04-06 18:56:48 +0300

    Day 15 exercise

--- main/scala/Day15Exercise.scala
+++ main/scala/Day15Exercise.scala
@@ -1,22 +1,20 @@
 object Day15Exercise extends App {
-  def getDay(day: Int): Unit = {
+  def getDay(day: Int): String = {
     day match {
-      case 1 => println("Monday")
-      case 2 => println("Tuesday")
-      case 3 => println("Wednesday")
-      case 4 => println("Thursday")
-      case 5 => println("Friday")
-      case 6 => println("Saturday !")
-      case 7 => println("Sunday !")
-      case whoa => println("No such Day!: ")
+      case 1 => ("Monday")
+      case 2 => ("Tuesday")
+      case 3 => ("Wednesday")
+      case 4 => ("Thursday")
+      case 5 => ("Friday")
+      case 6 => ("Saturday !")
+      case 7 => ("Sunday !")
+      case whoa => ("No such Day!: ")
     }
   }
-
   def getDayType(day: String): String = day match {
     case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday" => "WorkDay !"
     case "Saturday" | "Sunday" => "Weekend !"
   }
-
   println(getDayType("Monday"))
   println(getDay(4))
 }
